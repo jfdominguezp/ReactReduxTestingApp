3b951eaa098d27a64298fdbab99ef85c6af7e359
Juan Domínguez <juan@buziapp.com>
2018-07-13 14:13:15 -0500

    Reducers, routes and action for Auth HOC

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
+import * as actions from 'actions';
 
-const App = () => {
-  return (
-    <div>
-      <CommentBox />
-      <CommentList />
-    </div>
-  );
-};
+class App extends Component {
+  renderButton() {
+    if (this.props.auth) {
+      return (
+        <button onClick={() => this.props.changeAuth(false)}>
+          Sign out
+        </button>
+      );
+    }
 
-export default App;
+    return (
+      <button onClick={() => this.props.changeAuth(true)}>
+        Sign in
+      </button>
+    );
+  }
+
+  renderHeader() {
+    return (
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/post">Post a comment</Link></li>
+        <li><Link to="/">{this.renderButton()}</Link></li>
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        { this.renderHeader() }
+        <Route path="/post" component={CommentBox} />
+        <Route path="/" component={CommentList}/>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ auth }) => {
+  return { auth };
+}
+
+export default connect(mapStateToProps, actions)(App);
